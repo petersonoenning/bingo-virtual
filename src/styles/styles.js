a10d4da37e14ab8d4c5d1ea33f69d5cf24edb793
Peterson de Almeida Oenning <oenningpeterson9@gmail.com>
2025-02-15 16:30:56 -0400

    atualizando cor de fundo em styles.js

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#282c34',
+    backgroundColor: '#001f3f',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 15,
